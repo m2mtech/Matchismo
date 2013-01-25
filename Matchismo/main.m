83a5bd624d3302a4dc5e2e1d9b1e15848246167a
m2m tech <tech@m2m.at>
2013-01-25 13:24:49 +0100

    Initial Commit

---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Martin Mandl on 25.01.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
