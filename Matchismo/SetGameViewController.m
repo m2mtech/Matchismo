8822d7ac6c5b5e9664070567df27f74eb5bc193b
m2m tech <tech@m2m.at>
2013-02-16 01:44:03 +0100

    assignment #2 task #6

--- Matchismo/SetGameViewController.m
+++ Matchismo/SetGameViewController.m
@@ -8,6 +8,7 @@
 
 #import "SetGameViewController.h"
 #import "SetCardDeck.h"
+#import "SetCard.h"
 
 @interface SetGameViewController ()
 
@@ -28,11 +29,56 @@
     return _game;
 }
 
+- (NSAttributedString *)updateAttributedString:(NSAttributedString *)attributedString withAttributesOfCard:(SetCard *)card
+{
+    NSMutableAttributedString *mutableAttributedString = [attributedString mutableCopy];
+    
+    NSRange range = [[mutableAttributedString string] rangeOfString:card.symbol];
+    if (range.location != NSNotFound) {
+        NSString *symbol = @"?";
+        if ([card.symbol isEqualToString:@"oval"]) symbol = @"●";
+        if ([card.symbol isEqualToString:@"squiggle"]) symbol = @"▲";
+        if ([card.symbol isEqualToString:@"diamond"]) symbol = @"■";
+       
+        NSMutableDictionary *attributes = [[NSMutableDictionary alloc] init];
+        
+        if ([card.color isEqualToString:@"red"])
+            [attributes setObject:[UIColor redColor] forKey:NSForegroundColorAttributeName];
+        if ([card.color isEqualToString:@"green"])
+            [attributes setObject:[UIColor greenColor] forKey:NSForegroundColorAttributeName];
+        if ([card.color isEqualToString:@"purple"])
+            [attributes setObject:[UIColor purpleColor] forKey:NSForegroundColorAttributeName];
+
+        if ([card.shading isEqualToString:@"solid"])
+            [attributes setObject:@-5 forKey:NSStrokeWidthAttributeName];
+        if ([card.shading isEqualToString:@"striped"])
+            [attributes addEntriesFromDictionary:@{
+                     NSStrokeWidthAttributeName : @-5,
+                     NSStrokeColorAttributeName : attributes[NSForegroundColorAttributeName],
+                     NSForegroundColorAttributeName : [attributes[NSForegroundColorAttributeName] colorWithAlphaComponent:0.1]
+             }];
+        if ([card.shading isEqualToString:@"open"])
+            [attributes setObject:@5 forKey:NSStrokeWidthAttributeName];
+
+        symbol = [symbol stringByPaddingToLength:card.number withString:symbol startingAtIndex:0];
+        [mutableAttributedString replaceCharactersInRange:range
+                                     withAttributedString:[[NSAttributedString alloc] initWithString:symbol
+                                                                                          attributes:attributes]];
+    }
+    
+    return mutableAttributedString;
+}
+
 - (void)updateUI
 {
     for (UIButton *cardButton in self.cardButtons) {
         Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
-        [cardButton setTitle:card.contents forState:UIControlStateNormal];
+        NSAttributedString *title = [[NSAttributedString alloc] initWithString:card.contents];
+        if ([card isKindOfClass:[SetCard class]]) {
+            SetCard *setCard = (SetCard *)card;
+            title = [self updateAttributedString:title withAttributesOfCard:setCard];
+        }        
+        [cardButton setAttributedTitle:title forState:UIControlStateNormal];
         cardButton.selected = card.isFaceUp;
         cardButton.enabled = !card.isUnplayable;
         cardButton.alpha = (card.isUnplayable ? 0.3 : 1.0);
